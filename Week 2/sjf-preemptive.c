2b173649d7f39d6c901b00efa78759324dbd61ab
Rishi Kumar Chourasia <rishikumar.cs24@bmsce.ac.in>
2026-03-04 11:38:01 +0530

    Add files via upload

---
+++ Week 2/sjf-preemptive.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int main() {
+    int n, bt[20], wt[20], tat[20], at[20], ct[20], completed[20];
+    int i, j, time = 0, min, pos;
+    float twt = 0.0, ttat = 0.0, awt, att;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    // Input Arrival Time and Burst Time
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter Arrival Time for Process %d: ", i + 1);
+        scanf("%d", &at[i]);
+        printf("Enter Burst Time for Process %d: ", i + 1);
+        scanf("%d", &bt[i]);
+        completed[i] = 0;
+    }
+
+    int count = 0;
+
+    while (count < n)
+        {
+        min = 9999;
+        pos = -1;
+
+        // Find shortest job among arrived processes
+        for (i = 0; i < n; i++)
+        {
+            if (at[i] <= time && completed[i] == 0)
+            {
+                if (bt[i] < min)
+                {
+                    min = bt[i];
+                    pos = i;
+                }
+            }
+        }
+
+        if (pos == -1) {
+            time++; // CPU idle
+        }
+        else
+        {
+            time += bt[pos];
+            ct[pos] = time;
+            tat[pos] = ct[pos] - at[pos];
+            wt[pos] = tat[pos] - bt[pos];
+
+            completed[pos] = 1;
+            count++;
+        }
+    }
+
+    // Calculate averages
+    for (i = 0; i < n; i++)
+    {
+        twt += wt[i];
+        ttat += tat[i];
+    }
+
+    awt = twt / n;
+    att = ttat / n;
+
+    // Output Table
+    printf("\nPROCESS\tAT\tBT\tCT\tWT\tTAT");
+    for (i = 0; i < n; i++)
+    {
+        printf("\nP%d\t%d\t%d\t%d\t%d\t%d",i + 1, at[i], bt[i], ct[i], wt[i], tat[i]);
+    }
+
+    printf("\n\nAverage Waiting Time = %.2f", awt);
+    printf("\nAverage Turnaround Time = %.2f\n", att);
+
+    return 0;
+}
