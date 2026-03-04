aa4c43fb9757eb37748f69db4e45072193471c14
Rishi Kumar Chourasia <rishikumar.cs24@bmsce.ac.in>
2026-03-04 12:01:38 +0530

    Update sjf-non-preemptive.c

--- Week 2/sjf-non-preemptive.c
+++ Week 2/sjf-non-preemptive.c
@@ -1,63 +1,63 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main() {
-    int n, bt[20], rt[20], wt[20], tat[20], at[20], ct[20];
-    int i, time = 0, count = 0, min, pos;
+    int n, bt[20], at[20], wt[20], tat[20], ct[20];
+    int pid[20], completed[20] = {0};
+    int i, time = 0, count = 0, pos;
     float twt = 0.0, ttat = 0.0, awt, att;
 
-    printf("Enter the number of processes: ");
+    printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    // Input Arrival Time and Burst Time
-    for (i = 0; i < n; i++)
-    {
+    // Input
+    for (i = 0; i < n; i++) {
+        pid[i] = i + 1;
+
         printf("Enter Arrival Time for Process %d: ", i + 1);
         scanf("%d", &at[i]);
+
         printf("Enter Burst Time for Process %d: ", i + 1);
         scanf("%d", &bt[i]);
-        rt[i] = bt[i]; // Remaining time
     }
 
-    while (count < n)
-        {
-        min = 9999;
+    // Non-Preemptive SJF Scheduling
+    while (count < n) {
+        int min = INT_MAX;
         pos = -1;
 
-        // Find process with shortest remaining time
-        for (i = 0; i < n; i++)
-        {
-            if (at[i] <= time && rt[i] > 0)
-            {
-                if (rt[i] < min)
-                {
-                    min = rt[i];
+        // Find shortest job among arrived processes
+        for (i = 0; i < n; i++) {
+            if (at[i] <= time && completed[i] == 0) {
+                if (bt[i] < min) {
+                    min = bt[i];
                     pos = i;
+                }
+                // Tie breaker: earlier arrival time
+                else if (bt[i] == min) {
+                    if (at[i] < at[pos]) {
+                        pos = i;
+                    }
                 }
             }
         }
 
-        if (pos == -1)
-        {
-            time++; // CPU idle
+        if (pos == -1) {
+            time++;  // CPU idle
         }
-        else
-        {
-            rt[pos]--;   // Execute process for 1 unit
-            time++;
+        else {
+            time += bt[pos];      // Run full burst time
+            ct[pos] = time;
+            tat[pos] = ct[pos] - at[pos];
+            wt[pos] = tat[pos] - bt[pos];
 
-            if (rt[pos] == 0)
-            {
-                count++;
-                ct[pos] = time;
-                tat[pos] = ct[pos] - at[pos];
-                wt[pos] = tat[pos] - bt[pos];
-            }
+            completed[pos] = 1;
+            count++;
         }
     }
 
     // Calculate averages
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         twt += wt[i];
         ttat += tat[i];
     }
@@ -65,12 +65,11 @@ int main() {
     awt = twt / n;
     att = ttat / n;
 
-    // Output Table
+    // Output
     printf("\nPROCESS\tAT\tBT\tCT\tWT\tTAT");
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         printf("\nP%d\t%d\t%d\t%d\t%d\t%d",
-               i + 1, at[i], bt[i], ct[i], wt[i], tat[i]);
+               pid[i], at[i], bt[i], ct[i], wt[i], tat[i]);
     }
 
     printf("\n\nAverage Waiting Time = %.2f", awt);
